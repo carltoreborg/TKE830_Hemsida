cd8fe15db708d96f4b9549452393929a961c392b
Huml4n <h.forsling@gmail.com>
2024-09-26 17:15:27 +0200

    fixed image snark

--- snark-website/src/app/page.tsx
+++ snark-website/src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
+import snark_image from "../images/snark.png";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="front-header">
         IKEA SNARK
-        </div>
-      <image src="/snark.jpg" alt="IKEA SNARK" width={500} height={500} />
+      </div>
+      <Image src="/images/snark.png" 
+             alt="IKEA SNARK" 
+             width={500} 
+             height={500} />
     </div>
   );
 }
