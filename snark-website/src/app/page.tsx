361d9cd0399400341da8ea5485348ac9531d1ba8
Carl Toreborg <114061060+carltoreborg@users.noreply.github.com>
2024-09-30 22:27:07 +0200

    Fixed videoviewer and added slides function

--- snark-website/src/app/page.tsx
+++ snark-website/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Video from "./Video";
+import YoutubeEmbed from "./Video/text";
 import Info from "./components/Info";
 import OurTeam from "./components/Info/ourteam";
+import SlideShow from "./components/Info/SlideShow";
 import "./homepage.css";
 import Divider from '@mui/material/Divider';
 
@@ -11,15 +13,24 @@ export default function Home() {
         <div className="absolute inset-0 bg-black opacity-50"></div> {/* Overlay */}
 
       <div className="flex flex-col items-center">
-        <div className="positionbox element1">
+        <div className="positionbox element1 z-10 mb-20">
           <Info />
-          <Video />
         </div>
-        <div className="positionbox element2 z-10">
+
+        <div className="positionbox element2 z-10 mb-8">
+        <SlideShow />
+        </div>
+
+        <div className="positionbox element3 z-10 mb-8">
+        <YoutubeEmbed embedId="_6z3CIG_tSg?si=W7HbwlmUDJVsxF_n" />
+        {/*<Video />*/}
+        </div>
+
+        <div className="positionbox element4 z-10">
           <h1 className="text-8xl text-white flex flex-col items-center justify-center mb-8">Our Team</h1>
-          <Divider orientation="vertical" flexItem />
           <OurTeam />
         </div>
+        
       </div>
     </section>
   );
