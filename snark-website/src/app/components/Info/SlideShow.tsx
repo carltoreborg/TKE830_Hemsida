ecb83125d65451ee6d79a8bc9bba936f8a614e78
Simon Westlin Green <simon.green@live.se>
2024-10-03 12:14:14 +0200

    ERROR ERROR ERROR ERROR ERROR ERROR

--- snark-website/src/app/components/Info/SlideShow.tsx
+++ snark-website/src/app/components/Info/SlideShow.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import ReactGoogleSlides from "react-google-slides";
 
 const SlideShow = () => {
   return (
-      <div>
-      <iframe src={"./slides/digital_utbildning.pdf"} width={'1000px'} height={'500px'} />
-      </div>
-      );
-     };
+    <div>
+      <iframe
+        src={"./slides/digital_utbildning.pdf"}
+        width={"1000px"}
+        height={"500px"}
+      />
+    </div>
+  );
+};
 
 export default SlideShow;
--- snark-website/src/app/page.tsx
+++ snark-website/src/app/page.tsx
@@ -1,4 +1,3 @@
-import YoutubeEmbed from "./components/Video/text";
 import Info from "./components/Info";
 import OurTeam from "./components/Info/ourteam";
 import SlideShow from "./components/Info/SlideShow";
@@ -8,40 +7,45 @@ import "./homepage.css";
 
 export default function Home() {
   return (
-    <section className="relative flex items-center justify-center min-h-screen bg-cover bg-center" 
-      style={{ backgroundImage: 'url(/images/background.jpg)' }}>
-        <div className="absolute inset-0 bg-black opacity-50"></div> {/* Overlay */}
-
-
-
-
+    <section
+      className="relative flex items-center justify-center min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url(/images/background.jpg)" }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>{" "}
+      {/* Overlay */}
       <div className="flex flex-col items-center">
-
-      <div className="positionbox element1 z-10 mb-20">
-      <Header />
-      </div>
+        <div className="positionbox element1 z-10 mb-20">
+          <Header />
+        </div>
 
         <div className="positionbox element1 z-10 mb-20">
-          
           {/*<YoutubeEmbed embedId="_6z3CIG_tSg?si=W7HbwlmUDJVsxF_n" />*/}
         </div>
 
         <div className="positionbox element2 z-10 mb-8 flex flex-col items-center justify-center">
-          <h1 className="text-6xl text-white flex flex-col items-center justify-center mb-8 mt-20">Sprint 2 - Second Pitch Presentation</h1>
+          <h1 className="text-6xl text-white flex flex-col items-center justify-center mb-8 mt-20">
+            Sprint 2 - Second Pitch Presentation
+          </h1>
           <SlideShow />
         </div>
 
         <div className="positionbox element3 z-10 mb-8 flex flex-col items-center justify-center">
-          <h1 className="text-6xl text-white flex flex-col items-center justify-center mb-8 mt-20">Important Aspects of the Product Design</h1>
+          <h1 className="text-6xl text-white flex flex-col items-center justify-center mb-8 mt-20">
+            Important Aspects of the Product Design
+          </h1>
           <Info />
         </div>
 
         <div className="positionbox element4 z-10">
-          <h1 className="text-6xl text-white flex flex-col items-center justify-center mb-2 mt-20">Our Team</h1>
-          <h4 className="text-2xl text-white flex flex-col items-center justify-center mb-5">Click to show contact info</h4>
+          <h1 className="text-6xl text-white flex flex-col items-center justify-center mb-2 mt-20">
+            Our Team
+          </h1>
+          <h4 className="text-2xl text-white flex flex-col items-center justify-center mb-5">
+            Click to show contact info
+          </h4>
           <OurTeam />
         </div>
-        
+
         <div className="positionbox element5 z-10 mt-8">
           <Footer />
         </div>
